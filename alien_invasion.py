713d4a27f54030d8b1fefa15ef85bb04b829d2c2
guang <guang@369.com>
2022-10-06 00:07:25 +0800

    开火

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlenInvasion:
     """管理游戏资源和行为的类"""
@@ -12,11 +13,14 @@ class AlenInvasion:
         #设置类
         self.settings = Settings()
         #设置game窗口大小
-        self.screen = pygame.display.set_mode((self.settings.screen_widht, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((self.settings.screen_widht, self.settings.screen_height),pygame.RESIZABLE)
         #设置game窗口标题
         pygame.display.set_caption("Alien Invasion")
         #创建一搜飞船
         self.ship = Ship(self)
+        #创建一个存储子弹的编组
+        self.bullets = pygame.sprite.Group()
+
 
 
     def run_game(self):
@@ -28,11 +32,14 @@ class AlenInvasion:
             #飞船移动后位置更新
             self.ship.update()
 
-            # # 每次循环时重绘屏幕
+            #更新 bullets 编组中所有子弹的位置
+            self.bullets.update()
+
+            #每次循环时重绘屏幕
             self._undate_screen()
 
-            #让最近绘制的屏幕可见
-            pygame.display.flip()
+            
+
 
     """重构事件监视和屏幕更新的代码"""
     def _check_events(self):
@@ -40,28 +47,51 @@ class AlenInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            elif event.type == pygame.KEYDOWN:                                 #触发按键 按下 事件
-                if event.key == pygame.K_RIGHT or event.key == pygame.K_d:     #触发飞船右移按键(K_RIGHT)事件
-                    self.ship.moving_right = True                              #将右移标志置为 True
+            elif event.type == pygame.KEYDOWN:      #触发按键 按下 事件
+                self._check_keydown_events(event)   #处理键盘按键按下事件
 
-                elif event.key == pygame.K_LEFT or event.key == pygame.K_a:    #触发飞船左移按键(K_LEFT)事件
-                    self.ship.moving_left = True                               #将左移标志置 TRUE
+            elif event.type == pygame.KEYUP:        #触发按键 松开 事件
+                self._check_keyup_events(event)     #处理键盘按键松开事件
 
-            elif event.type == pygame.KEYUP:    #触发按键 抬起 事件
-                if event.key == pygame.K_RIGHT or event.key == pygame.K_d:       
-                    self.ship.moving_right = False
+    def _check_keydown_events(self,event):
+        """响应键盘按键按下"""
+        if event.key == pygame.K_RIGHT or event.key == pygame.K_d:     #触发飞船右移按键(K_RIGHT or K_d)事件
+            self.ship.moving_right = True                              #将右移标志置为 True
 
-                if event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                    self.ship.moving_left = False
+        elif event.key == pygame.K_q:
+            sys.exit()                              
 
-    
+        elif event.key == pygame.K_LEFT or event.key == pygame.K_a:    #触发飞船左移按键(K_LEFT or K_a)事件
+            self.ship.moving_left = True                               #将左移标志置为 True
+        
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
+    def _check_keyup_events(self,event):
+        """响应键盘按键松开"""
+        if event.key == pygame.K_RIGHT or event.key == pygame.K_d:     #触发飞船右移按键(K_RIGHT or K_d)松开事件  
+            self.ship.moving_right = False                             #将右移标志置为 False
+
+        if event.key == pygame.K_LEFT or event.key == pygame.K_a:      #触发飞船左移按键(K_LEFT or K_a)松开事件
+            self.ship.moving_left = False                              #将左移标志置为 False
+
+    def _fire_bullet(self):
+        """创建一颗子弹，并将其加入编组 bullets 中"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _undate_screen(self):
         """更新屏幕上的图像，并切换到更新屏幕"""
         # 每次循环时重绘屏幕
         self.screen.fill(self.settings.bg_color)
         #在窗口显示飞船
         self.ship.blitme()
-
+        #在窗口中显示所有子弹
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+    
+        #让最近绘制的屏幕可见
+        pygame.display.flip()
 
 
 if __name__ == '__main__' :
--- settings.py
+++ settings.py
@@ -2,13 +2,20 @@ import pygame
 
 class Settings:
     def __init__(self):
-        #窗口设置
+        """窗口设置"""
         #设置游戏窗口大小
         self.screen_widht = 1200
         self.screen_height = 800
         #设置背景颜色
         self.bg_color = (230, 230, 230)
 
-        #飞船设置
+        """飞船设置"""
         self.ship_speed = 1.5   #飞船速度
+
+        """子弹设置"""
+        self.bullet_speed = 1.0         #子弹速度
+        self.bullet_width = 3           #子弹宽度
+        self.bullet_height = 15         #子弹长度
+        self.bullet_color = (60,60,60)  #子弹颜色
+
 
